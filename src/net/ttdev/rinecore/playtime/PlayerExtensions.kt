3c4eb6869778e7ef5d0452ad16e176b67b90b898
TTGTRE <tr3.logan@gmail.com>
2018-10-22 20:56:53 -0400

    use new api methods for saving maps

--- src/net/ttdev/rinecore/playtime/PlayerExtensions.kt
+++ src/net/ttdev/rinecore/playtime/PlayerExtensions.kt
@@ -5,39 +5,38 @@ import java.time.Duration
 
 
 var Player.playtime: Long
-get() = getPlaytimeSeconds()
-set(value) = setPlaytimeSeconds(value)
+    get() = getPlaytimeSeconds()
+    set(value) = setPlaytimeSeconds(value)
 
 /**
  * The players playtime in seconds.
  */
 fun Player.getPlaytimeSeconds(): Long {
-    val playTimeList = getPlaytimeEntries()
-    val filteredList = playTimeList.filter { it.contains(uniqueId.toString()) }
+    /* If the entry exists, then return the playtime
+    * associated with that entry, otherwise return 0
+    * indicating that there is no entry for that UUID */
+    val entries = getPlaytimeEntries()
 
-    /* this player doesn't have a playtime yet even though they're
-        playing on the server. that's not right, so return -1 to
-        indicate an error has occurred.
-         */
-    if (filteredList.isEmpty()) return -1
-
-    return filteredList[0].split("->")[1].toLong()
-}
-
-fun Player.setPlaytimeSeconds(seconds: Long) {
-    val playtimeList = getPlaytimeEntries()
-    val filteredList = playtimeList.filter { it.contains(uniqueId.toString()) }
-
-    if (filteredList.isEmpty()) {
-        playtimeList.add("$uniqueId->0")
-        setPlaytimeEntries(playtimeList)
-        return
+    if (entries[uniqueId] == null) {
+        println("entry is null")
+        entries[uniqueId] = 0
     }
 
-    val newEntry = "$uniqueId->$seconds"
-    playtimeList.removeAll(filteredList) // Remove old entries
-    playtimeList.add(newEntry)
-    setPlaytimeEntries(playtimeList)
+    return entries[uniqueId] ?: 0
+}
+
+/**
+ * Set the playtime for this player in seconds.
+ */
+fun Player.setPlaytimeSeconds(seconds: Long) {
+    val entries = getPlaytimeEntries()
+
+    /* Create a new entry for the player with the new
+    * playtime */
+    entries[uniqueId] = seconds
+
+    /* Replace players old entry with new entry */
+    setPlaytimeEntries(entries)
 }
 
 /**
--- src/net/ttdev/rinecore/playtime/PlaytimeFunctions.kt
+++ src/net/ttdev/rinecore/playtime/PlaytimeFunctions.kt
@@ -1,21 +1,30 @@
 package net.ttdev.rinecore.playtime
 
+import getMap
 import net.ttdev.rinecore.main
+import setMap
+import java.util.*
 
 /**
  * Get the list of playtimes from the config.yml.
  * Each entry in the list looks like this:
  * `UUID->PLAYTIME (in seconds)`
  */
-fun getPlaytimeEntries(): MutableList<String> = main.config.getStringList("playtimes")
+fun getPlaytimeEntries() = main.config.getMap<UUID, Long>("playtimes", { UUID.fromString(it) }, { it.toString().toLong() })
 
-fun setPlaytimeEntries(entries: Collection<String>) {
-    main.config.set("playtimes", entries)
+/**
+ * Save `entries` to configuration, replacing
+ * all previous existing entries.
+ */
+fun setPlaytimeEntries(entries: MutableMap<UUID, Long>) {
+    val remapped = mutableMapOf<String, Long>()
+    entries.mapKeysTo(remapped) { it.key.toString() }
+    main.config.setMap("playtimes", remapped)
     main.saveConfig()
 }
 
 /**
- * All numbers are returned as their superscripted
+ * All numbers are returned as their superscript
  * counterparts.
  * E.g. 1234 becomes ¹²³⁴
  */
